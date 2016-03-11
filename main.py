d7bebf147af75965239733dea4b6a6d7ec7ccc34
rik91 <eric.canadas@gmail.com>
2016-03-11 11:17:48 +0100

    adding selector between episodes or seasons

--- main.py
+++ main.py
@@ -87,7 +87,10 @@ def search_season(info):
     return search_general(info)
 
 # This registers your module for use
-provider.register(search, search_movie, search_episode, search_season)
+if "false" == settings.value.get("episodes", "false"):
+    provider.register(search, search_movie, search_episode, search_season)
+else:
+    provider.register(search, search_movie, search_season, search_season)
 
 del settings
 del browser
